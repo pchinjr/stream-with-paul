33cb1e2958d4eac6bc23a66fd9d8498fdd8f71e5
Paul Chin Jr <pchinjr@gmail.com>
2020-03-05 11:34:54 -0500

    New image from new link

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -29,7 +29,7 @@ exports.handler = async function Index () {
     photographer: 'Ivana Cajina',
     service: 'Unsplash',
     credit: 'https://unsplash.com/@von_co',
-    image: 'https://imgur.com/gallery/FPR92li'
+    image: 'https://user-images.githubusercontent.com/10526646/76002768-1b763c80-5ed5-11ea-8ca6-8796e2d85362.jpg'
     // or link to an external image URL such as ↓
     // image: 'https://images.unsplash.com/photo-1506535772317-9fdb71c959c6'
   })
